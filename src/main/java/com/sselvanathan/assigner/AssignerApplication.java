42c6447179d596eb3105e546b0e078bb66fcfa9b
Senthuran Selvanathan <46525254+sselvanathan@users.noreply.github.com>
2023-09-09 09:45:11 +0200

    init

---
+++ src/main/java/com/sselvanathan/assigner/AssignerApplication.java
@@ -0,0 +1,13 @@
+package com.sselvanathan.assigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sselvanathan/assigner/AssignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sselvanathan.assigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
